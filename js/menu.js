c706513f16d644ea3ac5042fb19f5421c6edd8f9
JoseLuis400 <153861602+LanzamientosEspaciales@users.noreply.github.com>
2025-04-22 18:09:48 -0300

    Nuevo menú de carga

--- js/menu.js
+++ js/menu.js
@@ -39,6 +39,18 @@ document.addEventListener("DOMContentLoaded", () => {
 const loader = document.createElement("div");
 loader.id = "loader";
 loader.innerHTML = `
+    <!-- From Uiverse.io by wojtek_4284 --> 
+<div id="box">
+  <div id="l1">C</div>
+  <div id="l2">A</div>
+  <div id="l3">R</div>
+  <div id="l4">G</div>
+  <div id="l5">A</div>
+  <div id="l6">N</div>
+  <div id="l7">D</div>
+  <div id="l8">O</div>
+</div>
+
     <svg id="svg1" viewBox="25 25 50 50">
         <circle id="circle1" r="20" cy="50" cx="50"></circle>
     </svg>
@@ -50,6 +62,68 @@ document.body.insertAdjacentElement("afterbegin", loader);
 // Crear y agregar el CSS del loader dinámicamente
 const styles = document.createElement("style");
 styles.innerHTML = `
+    /* From Uiverse.io by wojtek_4284 */ 
+#box div {
+  display: inline-block;
+  margin: 5px;
+  font-size: 35px;
+  animation: 2s obrot linear infinite;
+}
+
+#box {
+  width: 500px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes obrot {
+  0% {
+    transform: rotateX(0);
+  }
+  12.5% {
+    transform: rotateX(90deg);
+  }
+  25% {
+    transform: rotateX(180deg);
+  }
+  37.5% {
+    transform: rotateX(270deg);
+  }
+  50% {
+    transform: rotateX(360deg);
+  }
+  100% {
+    transform: rotateX(360deg);
+  }
+}
+
+#box div:nth-child(1) {
+  animation-delay: 0s;
+}
+#box div:nth-child(2) {
+  animation-delay: 0.1s;
+}
+#box div:nth-child(3) {
+  animation-delay: 0.2s;
+}
+#box div:nth-child(4) {
+  animation-delay: 0.3s;
+}
+#box div:nth-child(5) {
+  animation-delay: 0.4s;
+}
+#box div:nth-child(6) {
+  animation-delay: 0.5s;
+}
+#box div:nth-child(7) {
+  animation-delay: 0.6s;
+}
+#box div:nth-child(8) {
+  animation-delay: 0.7s;
+}
+
     #loader {
         position: fixed;
         top: 0;
