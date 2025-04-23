c5010cb66d0ba5d0b54b665320cf7675e73dff0f
JoseLuis400 <153861602+LanzamientosEspaciales@users.noreply.github.com>
2025-04-22 21:13:38 -0300

    Update bloquear_pagina.js

--- admin/telemetria/js/bloquear_pagina.js
+++ admin/telemetria/js/bloquear_pagina.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Estilos CSS
     const style = document.createElement("style");
     style.textContent = `
+        body {
+            overflow-y: auto;
+        }
         #password-overlay {
             position: fixed;
             top: 0;
@@ -15,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
             justify-content: center;
             align-items: center;
             z-index: 9999;
-            overflow-y: auto;
         }
 
         .password-box {
