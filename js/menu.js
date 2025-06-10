bbee78f2eff5d06de7875308e7007393c04c6414
JoseLuis400 <153861602+LanzamientosEspaciales@users.noreply.github.com>
2025-06-10 01:52:43 -0300

    Analiticas de Google

--- js/menu.js
+++ js/menu.js
@@ -96,3 +96,24 @@ const loader = document.createElement("div");
  window.addEventListener("load", function () {
      document.getElementById("loader").style.display = "none";
  });
+
+
+ // 1. Crear el script async del gtag.js
+const gtagScript = document.createElement('script');
+gtagScript.async = true;
+gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-7S8NZ102SQ';
+
+// 2. Insertarlo en el <head>
+document.head.appendChild(gtagScript);
+
+// 3. Crear el segundo script con el código de configuración
+const inlineScript = document.createElement('script');
+inlineScript.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-7S8NZ102SQ');
+`;
+
+// 4. Insertarlo en el <head> también
+document.head.appendChild(inlineScript);
